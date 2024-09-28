385cd35372f3789b5a56e7e932a38079ee5ba451
Volodymyr Burmus <burmusvolod@gmail.com>
2024-09-28 15:11:46 +0200

    [14]: Added Security config and validation mechanisms

---
+++ backend/src/main/java/org/fleetassistant/backend/utils/DefaultExceptionHandler.java
@@ -0,0 +1,32 @@
+package org.fleetassistant.backend.utils;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class DefaultExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Problem> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        String errorMessage = fieldError != null ? fieldError.getDefaultMessage() : "Invalid request";
+        Problem problem = buildProblem(Status.BAD_REQUEST, "Invalid request", errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem);
+    }
+
+    private Problem buildProblem(Status status, String title, String detail) {
+        return Problem.builder()
+                .withStatus(status)
+                .withTitle(title)
+                .withDetail(detail)
+                .build();
+    }
+}
