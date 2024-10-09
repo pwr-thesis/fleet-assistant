a6cdde6b7aa765e06d4ec181fe2b64194ce6dc68
Maryush <maryushp@gmail.com>
2024-10-09 20:48:58 +0200

    [22-23-31-32-38]: build fix

--- frontend/generate-environment.sh
+++ frontend/generate-environment.sh
@@ -1,7 +1,8 @@
 # generate-environment.sh
-echo "Creating environment.ts"
+mkdir -p src/environments
 cat <<EOF > src/environments/environment.ts
 export const environment = {
+  production: ${PRODUCTION},
   apiUrl: 'API_URL',
   apiKey: 'API_KEY'
 };
