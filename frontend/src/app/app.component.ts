e495bebb4f1be1f28001ee630c01df8ce8d6e1cf
Maryush Padhol <maryushp@gmail.com>
2024-11-01 11:34:23 +0100

    [45]: FE Google Map API & Vehicle's last location (#48)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './common/components/header/header.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { FooterComponent } from './common/components/footer/footer.component';
 import { NgIf } from '@angular/common';
-import { Subject, takeUntil } from 'rxjs';
 import { NavbarService } from '../utilities/services/navbar.service';
 import { SidenavComponent } from './common/components/sidenav/sidenav.component';
 import { BottomNavComponent } from './common/components/bottom-nav/bottom-nav.component';
+import { environment } from '../environments/environment';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-root',
@@ -24,15 +25,53 @@ import { BottomNavComponent } from './common/components/bottom-nav/bottom-nav.co
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     showNavbar = true;
-    private destroy$ = new Subject<void>();
 
-    constructor(private navbarService: NavbarService) {
+    constructor(
+        private destroyRef: DestroyRef,
+        private navbarService: NavbarService,
+        private renderer: Renderer2
+    ) {
         this.navbarService.showNavbar$
-            .pipe(takeUntil(this.destroy$))
+            .pipe(takeUntilDestroyed(destroyRef))
             .subscribe((show) => {
                 this.showNavbar = show;
             });
+    }
+
+    ngOnInit(): void {
+        this.addGoogleMapsScript();
+    }
+
+    addGoogleMapsScript(): void {
+        const apiKey = environment.googleApiKey;
+
+        const scriptContent = `(g => {
+      var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window;
+      b = b[c] || (b[c] = {}); 
+      var d = b.maps || (b.maps = {}), 
+      r = new Set, e = new URLSearchParams, 
+      u = () => h || (h = new Promise(async(f, n) => {
+        await (a = m.createElement("script"));
+        e.set("libraries", [...r] + ""); 
+        for (k in g) 
+          e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); 
+        e.set("callback", c + ".maps." + q); 
+        a.src = \`https://maps.\${c}apis.com/maps/api/js?\` + e; 
+        d[q] = f; 
+        a.onerror = () => h = n(Error(p + " could not load.")); 
+        a.nonce = m.querySelector("script[nonce]")?.nonce || ""; 
+        m.head.append(a)
+      })); 
+      d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
+    })({
+      key: '${apiKey}',
+    });`;
+
+        const scriptElement = this.renderer.createElement('script');
+        scriptElement.type = 'text/javascript';
+        scriptElement.text = scriptContent;
+        this.renderer.appendChild(document.body, scriptElement);
     }
 }
