4cbe009551c5e2d3118d30f5674bb648b267fc08
Maryush <maryushp@gmail.com>
2024-11-01 11:11:22 +0100

    [71] Location Test Data

--- backend/src/main/java/org/fleetassistant/backend/location/LocationService.java
+++ backend/src/main/java/org/fleetassistant/backend/location/LocationService.java
@@ -3,8 +3,6 @@ package org.fleetassistant.backend.location;
 import lombok.RequiredArgsConstructor;
 import org.fleetassistant.backend.location.model.Location;
 import org.fleetassistant.backend.utils.EntityToDtoMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,8 +15,8 @@ public class LocationService {
         return entityToDtoMapper.locationToLocationDto(locationRepository.save(location));
     }
 
-    public Page<org.fleetassistant.backend.dto.Location> readAllByVehicleId(Long vehicleId, Pageable pageable) {
-        return locationRepository.findAllByVehicle_Id(vehicleId, pageable).map(entityToDtoMapper::locationToLocationDto);
+    public org.fleetassistant.backend.dto.Location readLastLocation(Long vehicleId) {
+        return entityToDtoMapper.locationToLocationDto(locationRepository.findFirstByVehicle_IdOrderByIdDesc(vehicleId));
     }
 
     public void deleteAllByVehicleId(Long vehicleId) {
