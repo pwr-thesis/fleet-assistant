d6c43edb25ffec5513d6352a7dfaf288547fb1da
Maryush <maryushp@gmail.com>
2024-11-04 20:33:51 +0100

    [89]: Token Expiration Logic

--- frontend/src/utilities/auth.interceptor.ts
+++ frontend/src/utilities/auth.interceptor.ts
@@ -1,7 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, EMPTY } from 'rxjs';
+import { SnackbarService } from './services/snackbar.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const token = localStorage.getItem('accessToken');
+    const router = inject(Router);
+    const snackbarService = inject(SnackbarService);
+
     let authReq = req.clone();
     if (!req.url.includes('google')) {
         authReq = token
@@ -13,5 +20,15 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
             : req;
     }
 
-    return next(authReq);
+    return next(authReq).pipe(
+        catchError((error) => {
+            if (error.status === 401) {
+                localStorage.removeItem('userInfo');
+                localStorage.removeItem('accessToken');
+                router.navigate(['']);
+                snackbarService.openSnackBar('Your session was expired!');
+            }
+            return EMPTY;
+        })
+    );
 };
