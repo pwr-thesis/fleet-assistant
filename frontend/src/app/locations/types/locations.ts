be32ee914dc0bbd5c7186adfabbec8072a6d915a
Maryush <maryushp@gmail.com>
2024-11-11 19:52:45 +0100

    [92]: Fixed test

--- frontend/src/app/locations/types/locations.ts
+++ frontend/src/app/locations/types/locations.ts
@@ -1,13 +1,5 @@
-import { Vehicle } from '../../vehicles/types/vehicles';
-
 export interface Location {
     id: number;
     longitude: number;
     latitude: number;
-}
-
-export interface CustomMarker {
-    position: google.maps.LatLngLiteral;
-    vehicle: Vehicle;
-    content: HTMLImageElement;
 }
