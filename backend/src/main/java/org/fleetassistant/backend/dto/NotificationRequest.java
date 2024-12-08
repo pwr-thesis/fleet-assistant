244e01492faf7b48e2686de066b6f7525cea0a12
troshchylo <troshchylo@ukr.net>
2024-12-08 14:25:36 +0100

    [83] Added title to notification

--- backend/src/main/java/org/fleetassistant/backend/dto/NotificationRequest.java
+++ backend/src/main/java/org/fleetassistant/backend/dto/NotificationRequest.java
@@ -3,5 +3,5 @@ package org.fleetassistant.backend.dto;
 import lombok.Builder;
 
 @Builder
-public record NotificationRequest(String message, String email) {
+public record NotificationRequest(String title, String message, String email) {
 }
